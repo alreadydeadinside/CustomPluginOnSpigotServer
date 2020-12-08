179856a39987075f10a57c8040f3d0d5eca164d7
Bohdan Vakaliuk <54852515+BoleanSwift@users.noreply.github.com>
2020-12-09 00:39:56 +0200

    add methods, that will notify user in console about enabling/disabling server register player events

---
+++ src/main/java/com/alreadydeadinside/custom_items_and_recipes_plugin/Plugin.java
@@ -0,0 +1,29 @@
+package com.alreadydeadinside.custom_items_and_recipes_plugin;
+
+import com.alreadydeadinside.custom_items_and_recipes_plugin.events.PluginEvents;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Bohdan Vakaliuk
+ * @version 08.12.2020
+ *
+ */
+
+
+public class Plugin extends JavaPlugin {
+
+    //TODO methods, that will notify user in console about enabling/disabling server (DONE)
+
+    @Override
+    public void onEnable(){
+        getServer().getPluginManager().registerEvents(new PluginEvents(), this);
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[CustomItemsAndRecipesPlugin] Plugin is Enabled!");
+    }
+    @Override
+    public void onDisable(){
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[CustomItemsAndRecipesPlugin] Plugin is Disabled!");
+    }
+
+}
