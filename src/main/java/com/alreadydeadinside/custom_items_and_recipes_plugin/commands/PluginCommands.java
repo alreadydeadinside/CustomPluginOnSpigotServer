52cfc2658d237e31af36a0868a6fb6f29a827b00
Bohdan Vakaliuk <54852515+BoleanSwift@users.noreply.github.com>
2020-12-09 15:49:42 +0200

    create commands.java file with heal and feed command logic

--- src/main/java/com/alreadydeadinside/custom_items_and_recipes_plugin/commands/PluginCommands.java
+++ src/main/java/com/alreadydeadinside/custom_items_and_recipes_plugin/commands/PluginCommands.java
@@ -1,4 +1,37 @@
 package com.alreadydeadinside.custom_items_and_recipes_plugin.commands;
 
-public class PluginCommands {
+import org.bukkit.attribute.Attribute;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author Bohdan Vakaliuk
+ * @version 09.12.2020
+ *
+ */
+
+public class PluginCommands implements CommandExecutor {
+    //TODO common commands in chat for our plugin
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(!(sender instanceof Player)){ return true; }
+        Player player = (Player) sender;
+
+        //heal command
+        if(cmd.getName().equalsIgnoreCase("heal")){
+            double maxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue();
+            player.setHealth(maxHealth);
+            return true;
+        }
+
+        //feed command
+        if (cmd.getName().equalsIgnoreCase("feed")){
+            player.setFoodLevel(20); //max feed value
+        }
+
+        return true;
+    }
 }
