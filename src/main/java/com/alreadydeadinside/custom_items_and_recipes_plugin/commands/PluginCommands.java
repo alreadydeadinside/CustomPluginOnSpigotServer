48283bfe9f53dd042fb54d8d42630e26d0fb43dc
Bohdan Vakaliuk <54852515+BoleanSwift@users.noreply.github.com>
2020-12-10 23:51:30 +0200

    add notification messages, when certain command is used

--- src/main/java/com/alreadydeadinside/custom_items_and_recipes_plugin/commands/PluginCommands.java
+++ src/main/java/com/alreadydeadinside/custom_items_and_recipes_plugin/commands/PluginCommands.java
@@ -17,19 +17,25 @@ public class PluginCommands implements CommandExecutor {
     //TODO common commands in chat for our plugin
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if(!(sender instanceof Player)){ return true; }
+        if(!(sender instanceof Player)){
+            //TODO add description to your commands
+            sender.sendMessage("Only players can use this command");
+            return true;
+        }
         Player player = (Player) sender;
 
         //heal command
         if(cmd.getName().equalsIgnoreCase("heal")){
             double maxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue();
             player.setHealth(maxHealth);
+            player.sendMessage("You have been healed!");
             return true;
         }
 
         //feed command
         if (cmd.getName().equalsIgnoreCase("feed")){
             player.setFoodLevel(20); //max feed value
+            player.sendMessage("You have been fed!");
         }
 
         return true;
